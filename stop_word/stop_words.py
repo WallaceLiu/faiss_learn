17e1c1380903c2cc98398d28f55a9e12350580cc
liuning11 <liuning11@jd.com>
2020-04-11 20:56:14 +0800

    fixed

--- stop_word/stop_words.py
+++
@@ -1,7 +0,0 @@
-﻿def stop_word():
-    sw = []
-    with open('stop_words.txt', 'r') as fin:
-        lines = fin.readlines()
-    for line in lines:
-        sw.append(line.replace('\n', ''))
-    return sw
